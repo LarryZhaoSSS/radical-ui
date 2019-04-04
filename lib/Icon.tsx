914783240b987c1e6d8d72eab3ec464e72b8448e
LarryKameZhao <larryzhao1994@outlook.com>
2019-04-05 00:10:44 +0800

    add icons svg to the project and init the Icon component

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
+import  './fonts'
+
 interface IconProps {
   name: string
 }
 const Icon: React.FunctionComponent<IconProps> = props => {
-  return <span>{props.name}</span>
+  return (<span>{props.name}
+    <svg className="icon" aria-hidden="true">
+      <use xlinkHref={`#icon-${props.name}`}></use>
+</svg>
+  </span>)
 }
 export default Icon
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,4 @@
 import Icon from './Icon'
 import ReactDOM from 'react-dom'
 import React from 'react'
-ReactDOM.render(<Icon name="wechat"/>, document.body)
+ReactDOM.render(<div><Icon name="alipay" /></div>, document.body.querySelector('#root'))
