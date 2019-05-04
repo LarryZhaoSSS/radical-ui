27759b3b738ec5d6268ad1b692d436971b609f2f
LarryKameZhao <larryzhao1994@outlook.com>
2019-05-04 16:28:22 +0800

    new version 0.0.2

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,3 +2,4 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 ReactDOM.render(<div><App/></div>, document.body.querySelector('#root'))
+export { default as Icon } from './icon/Icon';
