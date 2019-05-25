9422dc5fddbab9a3ac6fc25b040e9461d9a9bcf1
LarryKameZhao <larryzhao1994@outlook.com>
2019-05-25 20:38:54 +0800

    style of the dialog and some events

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -5,9 +5,11 @@ export default function () {
   const [x, setX] = useState(false)
   return (
     <div>
-      <button onClick={()=>setX(!x)}>click</button>
-      <Dialog visible={x}>
-        <div>hi</div>
+      <button onClick={() => setX(!x)}>click</button>
+      <Dialog visible={x} closeOnClickMask={true} buttons={[<button onClick={()=>{setX(false)}}>cancel</button>, <button>ok</button>]}
+      onClose = {()=>{setX(false)}}
+      >
+        <div>main-content</div>
       </Dialog>
     </div>
   )
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,33 +1,53 @@
 import * as React from 'react'
 import './dialog.scss'
-import {Fragment} from 'react';
+import {Fragment, ReactElement} from 'react';
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
+import ReactDOM from 'react-dom';
 
 interface Props {
   visible: boolean
+  buttons: Array<ReactElement>
+  onClose: React.MouseEventHandler
+  closeOnClickMask?: boolean
 }
 
 const scopedClass = scopedClassMaker('r-parts-dialog')
 const sc = scopedClass
 const Dialog: React.FunctionComponent<Props> = (props) => {
-  return (
-    props.visible ?
-      <Fragment>
-        <div className={sc('mask')}></div>
-        <div className={sc()}>
-          <div className={sc('close')}>
-            <Icon name="close"/>
-          </div>
-          <header className={sc('header')}>title</header>
-          <main className={sc('main')}> {props.children}</main>
-          <footer className={sc('footer')}>
-            <button>ok</button>
-            <button>cancel</button>
-          </footer>
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e)
+
+  }
+  const onClickMask: React.MouseEventHandler = (e) => {
+    if (!props.closeOnClickMask) {
+      props.onClose(e)
+    }
+    return
+  }
+  const x = props.visible?<Fragment>
+      <div className={sc('mask')} onClick={onClickMask}></div>
+      <div className={sc()}>
+        <div className={sc('close')} onClick={onClickClose}>
+          <Icon name="close"/>
         </div>
-      </Fragment>
-      : null
+        <header className={sc('header')}>title</header>
+        <main className={sc('main')}> {props.children}</main>
+        <footer className={sc('footer')}>
+          {
+            props.buttons.map((button, index)=>{
+              React.cloneElement(button,{key:index})
+            })
+          }
+        </footer>
+      </div>
+    </Fragment>
+    : null
+  return (
+    ReactDOM.createPortal(x, document.body)
   )
+}
+Dialog.defaultProps={
+  closeOnClickMask: false
 }
 export default Dialog
