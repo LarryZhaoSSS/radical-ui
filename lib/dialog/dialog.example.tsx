9c06b9ad1bfd96dabe330ba7aff462628fefd808
LarryKameZhao <larryzhao1994@outlook.com>
2019-05-26 19:23:46 +0800

    finish the dialog apis

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
-import Dialog from "./dialog";
+import Dialog,{alert, confirm, modal} from "./dialog";
 
 export default function () {
   const [x, setX] = useState(false)
+  const openModal = () => {
+    const close = modal(<h1>你好，点我关闭<button onClick={()=>close()}>close</button></h1>)
+  }
   return (
     <div>
       <button onClick={() => setX(!x)}>click</button>
@@ -11,6 +14,10 @@ export default function () {
       >
         <div>main-content</div>
       </Dialog>
+      <button onClick={()=>{alert('hahaa')}}>alert trigger</button>
+      <button onClick={()=>{confirm('confirm content',()=>{console.log('点了yes')},()=>{console.log('点了no')})}}>confirm trigger</button>
+      <button onClick={()=>{modal(<h2>modal hi</h2>)}}>modal trigger</button>
+      <button onClick={openModal}>close in content</button>
     </div>
   )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import './dialog.scss'
-import {Fragment, ReactElement} from 'react';
+import {Fragment, ReactElement,  ReactNode} from 'react';
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
 import ReactDOM from 'react-dom';
 
 interface Props {
   visible: boolean
-  buttons: Array<ReactElement>
+  buttons?: Array<ReactElement>
   onClose: React.MouseEventHandler
   closeOnClickMask?: boolean
 }
@@ -25,7 +25,7 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     }
     return
   }
-  const x = props.visible?<Fragment>
+  const x = props.visible ? <Fragment>
       <div className={sc('mask')} onClick={onClickMask}></div>
       <div className={sc()}>
         <div className={sc('close')} onClick={onClickClose}>
@@ -33,13 +33,14 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
         </div>
         <header className={sc('header')}>title</header>
         <main className={sc('main')}> {props.children}</main>
-        <footer className={sc('footer')}>
-          {
-            props.buttons.map((button, index)=>{
-              React.cloneElement(button,{key:index})
-            })
-          }
-        </footer>
+        {
+          props.buttons && props.buttons.length>0 &&
+          <footer className={sc('footer')}>
+            {props.buttons && props.buttons.map((button, index) =>
+              React.cloneElement(button, {key: index})
+            )}
+          </footer>
+        }
       </div>
     </Fragment>
     : null
@@ -47,7 +48,49 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     ReactDOM.createPortal(x, document.body)
   )
 }
-Dialog.defaultProps={
-  closeOnClickMask: false
+Dialog.defaultProps = {
+  closeOnClickMask: false,
 }
+const modal = (content:  ReactNode ,buttons?: Array<ReactElement>, afterClose?:()=>void)=> {
+  const close = ()=> {
+    ReactDOM.render(React.cloneElement(component,{visible: false}), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+    afterClose && afterClose()
+  }
+  const component = <Dialog onClose={()=>{close();afterClose && afterClose()}}
+                            visible={true}
+                            buttons={buttons}
+  >
+    {content}
+  </Dialog>
+  const div = document.createElement('div')
+  document.body.append(div)
+  ReactDOM.render(component, div)
+  return close
+}
+const alert = (content: string) => {
+  const button =<button onClick={()=>close()}>ok</button>
+  const close = modal(content, [button])
+}
+const confirm = (content: string, yes?:()=>void, no?:()=>void)=>{
+
+  const onYes = () => {
+    close()
+    yes && yes()
+  }
+  const onNo = () => {
+    close()
+    no && no()
+  }
+
+  const buttons = [
+    <button onClick={onYes}>yes</button>,
+    <button onClick={onNo}>no</button>
+  ]
+  const close = modal(content,buttons)
+
+}
+
+export {alert, confirm, modal}
 export default Dialog
