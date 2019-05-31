f8990d359e2e10c14c4e2fd70d37db224c41d7ea
LarryKameZhao <larryzhao1994@outlook.com>
2019-06-01 07:49:24 +0800

    scope class refractor

--- lib/classes.tsx
+++ lib/classes.tsx
@@ -1,31 +1,22 @@
 interface Options {
-  extra: string  | undefined
+  extra: string | undefined
 }
+
 interface ClassToggles {
-  [key:string]: boolean
+  [key: string]: boolean
 }
 
-function scopedClassMaker(prefix: string) {
 
-  return function (name?: string | ClassToggles, options?:Options) {
-    // name = {hasAside: true, active: false,x: true}
-    let name2
-    let result = [prefix, name2].filter(Boolean).join('-')
-    if (typeof name === 'string' || name === undefined) {
-      name2 = [name]
-    } else {
-      name2 = Object.entries(name).filter(kv=>kv[1]).map(kv=>kv[0])
-    // ['hasAside','x']
-      // ['.r-parts-layout-hasAside','.r-parts-layout-x']
-    }
-    result = name2.map(name=>
-      [prefix, name].filter(Boolean).join('-')
-    ).join(' ')
-    if (options && options.extra) {
-      return [result,options && options.extra].filter(Boolean).join(' ')
-    } else {
-      return result
-    }
-  }
-}
+const scopedClassMaker = (prefix: string) =>
+  (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name : {[name]: name})
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean)
+        .join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
+
 export {scopedClassMaker}
--- lib/helpers/classnames.ts
+++ lib/helpers/classnames.ts
@@ -1,38 +1,27 @@
-function classes (...names: (string | undefined)[]) {
+function classes(...names: (string | undefined)[]) {
   return names.filter(Boolean).join(' ')
 }
 
 export default classes
 
 interface Options {
-  extra: string  | undefined
+  extra: string | undefined
 }
+
 interface ClassToggles {
-  [key:string]: boolean
+  [key: string]: boolean
 }
 
-function scopedClassMaker(prefix: string) {
 
-  return function (name?: string | ClassToggles, options?:Options) {
-    // name = {hasAside: true, active: false,x: true}
-    let name2
-    let result = [prefix, name2].filter(Boolean).join('-')
-    if (typeof name === 'string' || name === undefined) {
-      name2 = name
-      result = [prefix, name2].filter(Boolean).join('-')
-    } else {
-      name2 = Object.entries(name).filter(kv=>kv[1]).map(kv=>kv[0])
-      // ['hasAside','x']
-      result = name2.map(n=>{
-        return [prefix,n].filter(Boolean).join('-')
-      }).join(' ')
-      // ['.r-parts-layout-hasAside','.r-parts-layout-x']
-    }
-    if (options && options.extra) {
-      return [result,options && options.extra].filter(Boolean).join(' ')
-    } else {
-      return result
-    }
-  }
-}
+const scopedClassMaker = (prefix: string) =>
+  (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name : {[name]: name})
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean)
+        .join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
 export {scopedClassMaker}
