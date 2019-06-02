0c6a5e03f48510ed5da7104377309b0152a7bd1d
LarryKameZhao <larryzhao1994@outlook.com>
2019-06-02 23:13:03 +0800

    layout examples style

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -27,7 +27,7 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
   }
   const x = props.visible ? <Fragment>
       <div className={sc('mask')} onClick={onClickMask}></div>
-      <div className={sc()}>
+      <div className={sc('')}>
         <div className={sc('close')} onClick={onClickClose}>
           <Icon name="close"/>
         </div>
--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -4,41 +4,41 @@ import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
 import Aside from "./aside";
-
+import './layout.example.scss'
 export default function () {
   return (
     <div>
       <h1>第1个例子</h1>
-      <Layout style={{height:400}} className='hello'>
-        <Header>header</Header>
-        <Content>content</Content>
-        <Footer>footer</Footer>
+      <Layout style={{height:500,width: 500}} className='hello'>
+        <Header className='x'>header</Header>
+        <Content className='y'>content</Content>
+        <Footer className='x'>footer</Footer>
       </Layout>
       <h1>第2个例子</h1>
-      <Layout style={{height:400}}>
-        <Header>header</Header>
+      <Layout style={{height:500,width: 500}}>
+        <Header className='x'>header</Header>
         <Layout>
-          <Aside>aside</Aside>
-          <Content>content</Content>
+          <Aside className='z'>aside</Aside>
+          <Content className='y'>content</Content>
         </Layout>
-        <Footer>footer</Footer>
+        <Footer className='x'>footer</Footer>
       </Layout>
       <h1>第3个例子</h1>
-      <Layout style={{height:400}}>
-        <Header>header</Header>
+      <Layout style={{height:500,width: 500}}>
+        <Header className='x'>header</Header>
         <Layout>
-          <Content>content</Content>
-          <Aside>aside</Aside>
+          <Content className='y'>content</Content>
+          <Aside className='z'>aside</Aside>
         </Layout>
-        <Footer>footer</Footer>
+        <Footer className='x'>footer</Footer>
       </Layout>
       <h1>第4个例子</h1>
-      <Layout style={{height:400}}>
-        <Aside>aside</Aside>
+      <Layout style={{height:500,width: 500}}>
+        <Aside className='z'>aside</Aside>
         <Layout>
-          <Header>header</Header>
-          <Content>content</Content>
-          <Header>header</Header>
+          <Header className='x'>header</Header>
+          <Content className='y'>content</Content>
+          <footer className='x'>footer</footer>
         </Layout>
       </Layout>
     </div>
