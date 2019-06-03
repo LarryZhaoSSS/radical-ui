662b6e006f511b2a061b1a1d1267e8ce737a57df
LarryKameZhao <larryzhao1994@outlook.com>
2019-06-04 07:20:20 +0800

    add hight code render

--- demo.tsx
+++ demo.tsx
@@ -1,14 +1,45 @@
 import * as React from "react";
-interface Props{
+import Hightlight, {defaultProps} from 'prism-react-renderer'
+import {useState} from "react";
+
+interface Props {
   code: string
 }
+
 const Demo: React.FunctionComponent<Props> = (props) => {
+  const [codeVisible, setCodeVisible] = useState(false)
+  const code = (
+    <Hightlight {...defaultProps} code={props.code} language="jsx">
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={className} style={style}>
+            {
+              tokens.map((line, i) => (
+                <div {...getLineProps({line, key: i})}>
+                  {
+                    line.map((token, key) => (
+                      <span {...getTokenProps({token, key})}></span>
+                    ))
+                  }
+                </div>
+              ))
+            }
+          </pre>
+      )
+      }
+    </Hightlight>
+  )
   return (
     <div>
-      {props.children}
-      <pre>
-        {props.code}
-      </pre>
+      <div className='example'>
+        {props.children}
+      </div>
+      <div>
+        <button onClick={()=>{setCodeVisible(!codeVisible)}}>查看代码</button>
+      </div>
+      {
+        codeVisible &&  code
+      }
+
     </div>
   )
 }
