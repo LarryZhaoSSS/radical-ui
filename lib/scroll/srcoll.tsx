f8db2e02af46eaf3e1c7bf78a9aef7281aff58c9
LarryKameZhao <larryzhao1994@outlook.com>
2019-10-23 23:28:58 +0800

    scroll styles

--- lib/scroll/srcoll.tsx
+++ lib/scroll/srcoll.tsx
@@ -10,10 +10,14 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...rest} = props;
   const [barHeight,setBarHeight] = useState(0)
+  const [barTop, setBarTop] = useState(0)
   const onScroll: UIEventHandler = (e) => {
     console.log(e.currentTarget.scrollTop);
-
-
+    const { current } = containerRef
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    const scrollTop = current!.scrollTop
+    setBarTop(scrollTop * viewHeight / scrollHeight)
   };
   const containerRef = useRef<HTMLDivElement>(null)
   useEffect(()=>{
@@ -28,7 +32,7 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
            onScroll={onScroll}
       > {children}</div>
       <div className="r-parts-scroll-track">
-        <div className="r-parts-scroll-bar" style={{height:barHeight}}/>
+        <div className="r-parts-scroll-bar" style={{height:barHeight, transform:`translateY(${barTop}px)`}}/>
       </div>
     </div>
   );
