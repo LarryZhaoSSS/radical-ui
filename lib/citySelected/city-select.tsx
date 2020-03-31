58337e1e32f66ad3b799d88d51fda844d695756e
LarryKameZhao <larryzhao1994@outlook.com>
2020-03-31 23:35:02 +0800

    citySelect styles

--- lib/citySelected/city-select.tsx
+++ lib/citySelected/city-select.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useContext, Dispatch, SetStateAction } from 'react';
 import './city-select.scss';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
-interface Props {}
+import { useState, useEffect } from 'react';
+import pinyin from 'tiny-pinyin';
+interface Props {
+  dataSource: string[];
+  onChange: (p1: string) => void;
+}
+
+interface Context {
+  map: { [key: string]: string[] };
+  onChange: (p1: string) => void;
+  setDialogVisible: Dispatch<SetStateAction<boolean>>;
+}
+const CitySelectContext = React.createContext<Context>({
+  map: {},
+  onChange: (p1: string) => {},
+  setDialogVisible: () => {}
+});
+
 const CitySelect: React.FunctionComponent<Props> = props => {
   const [dialogVisible, setDialogVisible] = useState(true);
+  const map: Context['map'] = {};
+  props.dataSource.map(city => {
+    const py = pinyin.convertToPinyin(city);
+    const index = py[0];
+    map[index] = map[index] || [];
+    map[index].push(city);
+  });
   const onClick = () => {
     setDialogVisible(true);
   };
 
   return (
-    <>
+    <CitySelectContext.Provider
+      value={{ map, onChange: props.onChange, setDialogVisible }}
+    >
       <div onClick={onClick}>{props.children}</div>
       {dialogVisible && (
         <Dialog
@@ -19,22 +44,76 @@ const CitySelect: React.FunctionComponent<Props> = props => {
           }}
         />
       )}
-    </>
+    </CitySelectContext.Provider>
   );
 };
 const Dialog: React.FunctionComponent<{ onClose: () => void }> = props => {
+  const { map, onChange } = useContext(CitySelectContext);
+  console.log('---dialog---');
+  console.log(map);
+  const indexList = Object.keys(map).sort();
+  const cityList = Object.entries(map).sort(
+    (a, b) => a[0].charCodeAt(0) - b[0].charCodeAt(0)
+  );
+  const onClick = (city: string) => {
+    onChange(city);
+  };
   return ReactDOM.createPortal(
-    <div className='r-parts-citySelect-dialog' onClick={props.onClose}>
+    <div className='r-parts-citySelect-dialog'>
       <header>
-        <span className='icon'>&lt;</span>
+        <span className='icon' onClick={props.onClose}>
+          &lt;
+        </span>
         <span>选择城市</span>
       </header>
-      <div className='currentCity'></div>
+      <CurrentLocation />
       <h2>全部城市</h2>
-      <div className='cityIndex'>ABCD...</div>
-      <div className='cityList'>所有城市</div>
+      <ol className='r-parts-citySelect-index'>
+        {indexList.map(a => (
+          <li key={a}>{a}</li>
+        ))}
+      </ol>
+      <div className='cityList'>
+        {cityList.map(([letter, list]) => {
+          return (
+            <div key={letter} className='r-parts-citySelect-citySection'>
+              <h4>{letter}</h4>
+              {list.map(city => {
+                return (
+                  <div
+                    onClick={() => onClick(city)}
+                    className='r-parts-citySelect-cityName'
+                    key={city}
+                  >
+                    {city}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>,
     document.body
   );
+};
+const CurrentLocation: React.FunctionComponent = () => {
+  const [city, setCity] = useState<string>('加载中....');
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', 'http://ip-api.com/json/?lang=zh-CN');
+    xhr.onload = () => {
+      console.log(xhr.responseText);
+      const string = xhr.responseText;
+      const obj = JSON.parse(string);
+      const c = obj.city;
+      setCity(c);
+    };
+    xhr.onerror = () => {
+      setCity('未知');
+    };
+    xhr.send();
+  }, []);
+  return <div className='currentCity'>当前城市{city}</div>;
 };
 export { CitySelect };
--- lib/citySelected/citySelect.example.tsx
+++ lib/citySelected/citySelect.example.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { CitySelect } from './city-select';
+import { cityList } from './city-name';
 
 const CitySelectExample: React.FunctionComponent = () => {
   return (
     <div>
       <div>
         <h2>第一个例子</h2>
-        <CitySelect>city</CitySelect>
+        <CitySelect
+          dataSource={cityList}
+          onChange={(cityName: string) => {
+            console.log(cityName);
+          }}
+        >
+          city
+        </CitySelect>
       </div>
     </div>
   );
