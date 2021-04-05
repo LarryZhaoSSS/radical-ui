d88e8219bcf2fb487aea379df08d273ca834264f
LarryKameZhao <larryzhao1994@outlook.com>
2021-04-05 21:34:30 +0800

    update doc.sh

--- doc.sh
+++ doc.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 yarn doc
-git checkout gh-pages
+git push origin :gh-pages
+git checkout -b gh-pages
 mv - f doc/* ./
 git add .
 git commit -m "update gh-pages"
-git push
+git push origin gh-pages
 git checkout -
